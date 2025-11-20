8cc8717c0b6d77201ac3bd3ae241f1efbc290b77
K.Thoyajaksha Kashyap <kristipatithoyajakshakashyap@gmail.com>
2025-11-19 23:46:58 -0600

    Data Ingestion Component

--- networksecurity/exception/exception.py
+++ networksecurity/exception/exception.py
@@ -1,7 +1,7 @@
 import sys
 
 
-class NetworkSecuirtyException(Exception):
+class NetworkSecurityException(Exception):
     def __init__(self,error_message,error_detail:sys):
         super().__init__(error_message)
         self.error_message=error_message
