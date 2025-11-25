e72b10461a274e4def705020d6da7a9dc55dc85f
K.Thoyajaksha Kashyap <kristipatithoyajakshakashyap@gmail.com>
2025-11-25 16:36:50 -0600

    Data Transformation

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.components.data_validation import DataValidation
+from networksecurity.components.data_transformation import DataTransformation
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
-from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig
+from networksecurity.entity.config_entity import DataIngestionConfig,DataValidationConfig,DataTransformationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
 import sys
 
 if __name__=="__main__":
+    sys.dont_write_bytecode=True
     try:
         trainingpipelineconfig=TrainingPipelineConfig()
         dataingestionconfig=DataIngestionConfig(training_pipeline_config=trainingpipelineconfig)
@@ -21,6 +23,12 @@ if __name__=="__main__":
         datavalidationartifact=datavalidation.initiate_data_validation()
         logging.info("Data Validation Completed")
         print(f"Data Validation Artifact {datavalidationartifact}")
+        datatransformationconfig=DataTransformationConfig(training_pipeline_config=trainingpipelineconfig)
+        datatransformation=DataTransformation(data_validation_artifact=datavalidationartifact,data_transformation_config=datatransformationconfig)
+        logging.info("Data transformation started")
+        datatransformationartifact=datatransformation.initiate_data_transformation()
+        print(f"Data Transformation Artifact {datatransformationartifact}")
+        logging.info("Data transformation completed")
     except Exception as e:
         raise NetworkSecurityException(e,sys)
 
--- networksecurity/constants/training_pipeline/__init__.py
+++ networksecurity/constants/training_pipeline/__init__.py
@@ -10,10 +10,8 @@ TARGET_COLUMN="Result"
 PIPELINE_NAME:str="NetworkSecurity"
 ARTIFACT_DIR:str="Atrifacts"
 FILE_NAME:str="phisingData.csv"
-
 TRAIN_FILE_NAME:str="train.csv"
 TEST_FILE_NAME:str="test.csv"
-
 SCHEMA_FILE_PATH:str=os.path.join("data_schema","schema.yaml")
 
 """
@@ -34,3 +32,17 @@ DATA_VALIDATION_VALID_DIR:str="validated"
 DATA_VALIDATION_INVALID_DIR:str="invalid"
 DATA_VALIDATION_DRIFT_REPORT_DIR:str="drift_report"
 DATA_VALIDATION_DRIFT_REPORT_FILE_NAME:str="report.yaml"
+PREPROCESSING_OBJECT_FILE_NAME:str="preprocessing.pkl"
+
+"""
+Data Transformation related constant start with DATA_TRANSFORMATION VAR NAME
+"""
+DATA_TRANSFORMATION_DIR_NAME:str="data_transformation"
+DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR:str="transformed"
+DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR:str="transformed_object"
+## knn imputer to replace nan values
+DATA_TRANSFORMATION_IMPUTER_PARAMS:dict={
+    "missing_values":np.nan,
+    "n_neighbors":3,
+    "weights":"uniform",
+}
