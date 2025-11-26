a8281aa3fe0abaff32139c7a142ab94613325c48
K.Thoyajaksha Kashyap <kristipatithoyajakshakashyap@gmail.com>
2025-11-25 22:56:16 -0600

    ML Flow

--- networksecurity/components/model_trainer.py
+++ networksecurity/components/model_trainer.py
@@ -16,6 +16,7 @@ from sklearn.ensemble import (
     GradientBoostingClassifier,
     RandomForestClassifier
 )
+import mlflow
 
 
 class ModelTrainer:
@@ -27,6 +28,17 @@ class ModelTrainer:
             self.data_transformation_artifact=data_transformation_artifact
         except Exception as e:
             raise NetworkSecurityException(e,sys)
+
+    def track_mlflow(self,best_model,classificationmetric):
+        with mlflow.start_run():
+            f1_score=classificationmetric.f1_score
+            precision_score=classificationmetric.precision_score
+            recall_score=classificationmetric.recall_score
+            mlflow.log_metric("f1_score",f1_score)
+            mlflow.log_metric("precision",precision_score)
+            mlflow.log_metric("recall_score",recall_score)
+            mlflow.sklearn.log_model(best_model,"model")
+
 
     def train_model(self,X_train,y_train,X_test,y_test):
         try:
@@ -69,18 +81,22 @@ class ModelTrainer:
                 list(model_report.values()).index(best_model_score)
             ]
             best_model=models[best_model_name]
+            # Training prediction
             y_train_pred=best_model.predict(X_train)
             classification_train_metric=get_classification_score(y_true=y_train,y_pred=y_train_pred)
-            ## Track the mlflow
-            
+            # Track the training experiments results with mlflow
+            self.track_mlflow(best_model,classification_train_metric)
+            # Test predictions
             y_test_pred=best_model.predict(X_test)
             classification_test_metric=get_classification_score(y_true=y_test,y_pred=y_test_pred)
+            # Track the test experiments results with mlflow
+            self.track_mlflow(best_model,classification_test_metric)
             preprocessor=load_object(file_path=self.data_transformation_artifact.transformed_object_file_path)
             model_dir_path=os.path.dirname(self.model_train_config.trained_model_file_path)
             os.makedirs(model_dir_path,exist_ok=True)
             Network_Model=NetworkModel(preprocessor=preprocessor,model=best_model)
             save_object(file_path=self.model_train_config.trained_model_file_path,obj=Network_Model)
-            ## Model Trainer Artifact
+            # Model Trainer Artifact
             model_trainer_artifact=ModelTrainerArtifact(trained_model_file_path=self.model_train_config.trained_model_file_path,
                                  train_metric_artifact=classification_train_metric,
                                  test_metric_artifact=classification_test_metric)
