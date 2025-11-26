90435775a76d566fd1cac98cd13d1df013c6405b
K.Thoyajaksha Kashyap <kristipatithoyajakshakashyap@gmail.com>
2025-11-25 22:59:36 -0600

    Ml Flow

--- networksecurity/constants/training_pipeline/__init__.py
+++ networksecurity/constants/training_pipeline/__init__.py
@@ -8,7 +8,7 @@ Defining common constant variable for training pipeline
 """
 TARGET_COLUMN="Result"
 PIPELINE_NAME:str="NetworkSecurity"
-ARTIFACT_DIR:str="Atrifacts"
+ARTIFACT_DIR:str="Artifacts"
 FILE_NAME:str="phisingData.csv"
 TRAIN_FILE_NAME:str="train.csv"
 TEST_FILE_NAME:str="test.csv"
